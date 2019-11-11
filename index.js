d10e4ce9fb130611dbe719639cee1df4c5b1d3fc
yogamhd <yogapratamamhd@gmail.com>
2019-11-11 14:12:07 +0700

    Second commit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("hello")
+const chalk = require('chalk')
+
+const name = 'Yoga Pratama'
+console.log(chalk.blue(name))
+
+const info = chalk.keyword('red')
+
+const names = ['Dio Hermawan', 'Riski Maulana', 'Zahra']
+names.forEach(name => {
+  console.log(info(name))
+})
+
+
+
+
+
+//console.log("hello")
